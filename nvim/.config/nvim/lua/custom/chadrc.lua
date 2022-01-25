b79047f7de5845743a143f0d13c1aa9e119d605b
Daniel Boll <danielboll.academico@gmail.com>
2022-01-25 10:31:41 -0300

    feat(nvim): yes sir
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+-- Add highlight to when in insert mode cursor be block if highlight green
+-- and in normal mode cursor be block if highlight blue
+vim.cmd[[
+ set guicursor=i:block
+ set guifont=Iosevka\ 12
+ set so=12
+]]
+
 M.options = {
   relativenumber = true,
 }
