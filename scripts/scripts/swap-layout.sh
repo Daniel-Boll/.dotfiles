dae90de153722fc123bdda522a91116cc165395d
Daniel Boll <danielboll.academico@gmail.com>
2024-06-11 11:29:45 -0300

    fix(libnotify): use more generic notify-send instead of dunstify
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- scripts/scripts/swap-layout.sh
+++ scripts/scripts/swap-layout.sh
@@ -8,9 +8,9 @@ variant=$(setxkbmap -query | grep -oP 'variant:\s*\K\w+')
 
 # If the variant is empty, set it to international
 if [ -z "$variant" ]; then
-	dunstify -r 5555 -u low "Keyboard Layout" "Switched to US International 🇧🇷"
+	notify-send "Keyboard Layout" "Switched to US International 🇧🇷"
 	setxkbmap us -variant intl
 else
-	dunstify -r 5555 -u low "Keyboard Layout" "Switched to US 🇺🇸"
+	notify-send "Keyboard Layout" "Switched to US 🇺🇸"
 	setxkbmap us
 fi
